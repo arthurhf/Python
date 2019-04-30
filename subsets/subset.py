76665b01515989f25baac4d4113110bfc3950988
arthurhf <35467511+arthurhf@users.noreply.github.com>
2019-04-30 07:53:26 -0300

    Updated subdataset
    Added np.random.choice and np.sort to cross validation

--- subsets/subset.py
+++ subsets/subset.py
@@ -1,5 +1,6 @@
 #creates a subdataset from an existing csv dataset
 import csv
+import numpy as np
 import random
 
 numLinha = 0
@@ -13,21 +14,34 @@ with open('multiTimeline.csv', 'r') as ficheiro:
         set.append(linha)
         numLinha += 1
 
-
     ficheiro.close()
 
 print("\n\n{} linhas no DataSet".format(numLinha))
 for index in set:
     print(index)
 
-ini = random.randrange(0, numLinha, 1)
-print("\n\n{} linhas no SubSet".format(ini))
+#ini = random.randrange(0, numLinha, 1)
+#print("\n\n{} linhas no SubSet".format(ini))
 
-for i in range(ini):
+#for i in range(ini):
+    #print(i)
+    #print(set[i])
+ #   subset.append(set[i])
+    #print(subset)
+
+#for indexx in subset:
+ #   print(indexx)
+
+amostraAt = np.sort(np.random.choice(numLinha, 5, replace=False))
+amostraInst = np.sort(np.random.choice(3, 3, replace=False))
+print(amostraAt, amostraInst)
+
+for i in amostraAt:
     #print(i)
     #print(set[i])
     subset.append(set[i])
-    #print(subset)
+    print(subset)
 
-for indexx in subset:
-    print(indexx)
+#subsetInst = np.array(set)[amostraInst,:]
+#subsetAtr = np.array(set)[:, amostraAt]
+
