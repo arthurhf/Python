588b01dcc3a5ac137fee6aae40b16d3bd38ac39a
arthurhf <35467511+arthurhf@users.noreply.github.com>
2019-05-13 21:25:02 -0300

    Classe subset
    Criação da classe subset para modularização

--- subsets/subset.py
+++ subsets/subset.py
@@ -1,47 +1,57 @@
-#creates a subdataset from an existing csv dataset
 import csv
 import numpy as np
-import random
+from hyperpipes import HyperPipes
 
-numLinha = 0
-set = []
-subset = []
 
-with open('multiTimeline.csv', 'r') as ficheiro:
-    reader = csv.reader(ficheiro, delimiter=',', quoting=csv.QUOTE_NONE)
+class Subset:
 
-    for linha in reader:
-        set.append(linha)
-        numLinha += 1
+    def createsubset(self, dataset, percentageatt, percentageinst):
 
-    ficheiro.close()
+        numLinha = 0
+        set = []
+        subset = []
 
-print("\n\n{} linhas no DataSet".format(numLinha))
-for index in set:
-    print(index)
+        with open('multiTimeline.csv', 'r') as ficheiro:
+            reader = csv.reader(ficheiro, delimiter=',', quoting=csv.QUOTE_NONE)
 
-#ini = random.randrange(0, numLinha, 1)
-#print("\n\n{} linhas no SubSet".format(ini))
+            for linha in reader:
+                set.append(linha)
+                numLinha += 1
 
-#for i in range(ini):
-    #print(i)
-    #print(set[i])
- #   subset.append(set[i])
-    #print(subset)
+            ficheiro.close()
 
-#for indexx in subset:
- #   print(indexx)
+        print("\n\n{} linhas no DataSet".format(numLinha))
+        for index in set:
+            print(index)
 
-amostraAt = np.sort(np.random.choice(numLinha, 5, replace=False))
-amostraInst = np.sort(np.random.choice(3, 3, replace=False))
-print(amostraAt, amostraInst)
+        # ini = random.randrange(0, numLinha, 1)
+        # print("\n\n{} linhas no SubSet".format(ini))
 
-for i in amostraAt:
-    #print(i)
-    #print(set[i])
-    subset.append(set[i])
-    print(subset)
+        # for i in range(ini):
+            # print(i)
+            # print(set[i])
+        #   subset.append(set[i])
+            # print(subset)
 
-#subsetInst = np.array(set)[amostraInst,:]
-#subsetAtr = np.array(set)[:, amostraAt]
+        # for indexx in subset:
+        #   print(indexx)
 
+        # amostraAt = np.sort(np.random.choice(percentageatt, 5, replace=False))??????
+        amostraAt = np.sort(np.random.choice(numLinha, 5, replace=False))
+        # amostraInst = np.sort(np.random.choice(percentageinst, 3, replace=False))???????????
+        amostraInst = np.sort(np.random.choice(3, 3, replace=False))
+
+        print("\n\n" + amostraAt + "\n\n" + amostraInst)
+
+        for i in amostraAt:
+            # print(i)
+            # print(set[i])
+            subset.append(set[i])
+            print(subset)
+
+        # subsetInst = np.array(set)[amostraInst,:]
+        # subsetAtr = np.array(set)[:, amostraAt]
+
+        # hyper = HyperPipes()?????
+        # hyper.fit()????????
+        # return hyper
