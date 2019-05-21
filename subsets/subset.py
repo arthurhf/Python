f4b3355fdf85971d437cdc918c24fdca74ccbabe
arthurhf <35467511+arthurhf@users.noreply.github.com>
2019-05-20 22:12:26 -0300

    Updated Subset

--- subsets/subset.py
+++ subsets/subset.py
@@ -15,43 +15,45 @@ class Subset:
             reader = csv.reader(ficheiro, delimiter=',', quoting=csv.QUOTE_NONE)
 
             for linha in reader:
+                ncol = len(linha)
                 set.append(linha)
                 numLinha += 1
-
             ficheiro.close()
-
-        print("\n\n{} linhas no DataSet".format(numLinha))
+        print("\n\n{} colunas no DataSet".format(ncol))
+        print("{} linhas no DataSet".format(numLinha))
         for index in set:
             print(index)
 
-        # ini = random.randrange(0, numLinha, 1)
-        # print("\n\n{} linhas no SubSet".format(ini))
+            # ini = random.randrange(0, numLinha, 1)
+            # print("\n\n{} linhas no SubSet".format(ini))
 
-        # for i in range(ini):
+            # for i in range(ini):
             # print(i)
             # print(set[i])
-        #   subset.append(set[i])
+            #   subset.append(set[i])
             # print(subset)
 
-        # for indexx in subset:
-        #   print(indexx)
+            # for indexx in subset:
+            #   print(indexx)
 
-        # amostraAt = np.sort(np.random.choice(percentageatt, 5, replace=False))??????
-        amostraAt = np.sort(np.random.choice(numLinha, 5, replace=False))
+        amostraAt = np.sort(np.random.choice(numLinha, round(numLinha * percentageatt), replace=False))
+        # amostraAt = np.sort(np.random.choice(numLinha, 5, replace=False))
         # amostraInst = np.sort(np.random.choice(percentageinst, 3, replace=False))???????????
-        amostraInst = np.sort(np.random.choice(3, 3, replace=False))
+        amostraInst = np.sort(np.random.choice(ncol, round(ncol * percentageinst), replace=False))
 
-        print("\n\n" + amostraAt + "\n\n" + amostraInst)
+        # print("\n\n" + amostraAt + "\n\n" + amostraInst)
 
         for i in amostraAt:
             # print(i)
             # print(set[i])
             subset.append(set[i])
-            print(subset)
+            print(i)
 
-        # subsetInst = np.array(set)[amostraInst,:]
-        # subsetAtr = np.array(set)[:, amostraAt]
+        print(subset)
 
-        # hyper = HyperPipes()?????
-        # hyper.fit()????????
-        # return hyper
+        subsetInst = np.array(set)[amostraInst, :]
+        subsetAtr = np.array(set)[:, amostraAt]
+
+        hyper = HyperPipes()
+        hyper.fit(subset[:, 0:ncol-1], subset[:, ncol-1])
+        return hyper
